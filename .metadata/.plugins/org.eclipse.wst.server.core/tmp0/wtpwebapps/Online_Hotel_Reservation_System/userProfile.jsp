8f7670fed10e5f56e4c0904f64ffd5ceac1ccbfe
Starboy117 <mhmammar19@gmail.com>
2025-05-13 23:06:06 -0700

    commit 12

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Online_Hotel_Reservation_System/userProfile.jsp
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Online_Hotel_Reservation_System/userProfile.jsp
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+  <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
 <%
 
--- Online_Hotel_Reservation_System/src/main/java/com/user/DatabaseCon.java
+++ Online_Hotel_Reservation_System/src/main/java/com/user/DatabaseCon.java
@@ -3,33 +3,23 @@ package com.user;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
-
 public class DatabaseCon {
 
-	  	private static String url="jdbc:mysql://localhost:3306/tendura_hotel";
-		private static String dbUser ="root";
-		private static String pass ="parasite";
-		private static Connection con;
-		
-		public static Connection getConnection() {
-			
-			try {
-				
-				Class.forName("com.mysql.jdbc.Driver");
-				con = DriverManager.getConnection(url, dbUser, pass);
-				
-				
-			}
-			catch(Exception e) {
-				
-				System.out.println("Database Connection failed!!!");
-			}
-			
-			
-			return con;
-			
-		}
+    private static String url = "jdbc:mysql://localhost:3306/tendura_hotel";
+    private static String dbUser = "root";
+    private static String pass = "parasite";
+    private static Connection con;
 
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); // Updated driver for MySQL 8+
+            con = DriverManager.getConnection(url, dbUser, pass);
+            System.out.println("Database connected successfully!");
+        } catch (Exception e) {
+            System.out.println("Database Connection failed!!!");
+            e.printStackTrace(); // This prints the actual error
+        }
 
+        return con;
+    }
 }
